485fab8ce594b287617186c0e09bcbf307e803a2
Mafhz <martinn.henriquez@gmail.com>
2020-08-16 18:07:47 -0500

    Se implementa gif de carga de envio y gif de envío de mail y se programa boton de reset

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 const btnEnviar= document.querySelector('#enviar');
-
+const formularioEnviar = document.getElementById('enviar-mail');
+const resetBtn = document.getElementById('resetBtn');
 
 // Event Listeners
 
@@ -17,6 +18,14 @@ function eventListeners() {
     email.addEventListener('blur', validarCampo)
     asunto.addEventListener('blur', validarCampo)
     mensaje.addEventListener('blur', validarCampo)
+
+
+    // Boton de enviar e el submit
+    // btnEnviar.addEventListener('click', enviarEmail); esta es una forma
+    formularioEnviar.addEventListener('submit', enviarEmail);
+
+    // Boton de reset
+    resetBtn.addEventListener('click', resetFormulario);
 
 }
 
@@ -58,6 +67,41 @@ function validarCampo() {
 
 }
 
+/// Resetear el formulario
+function resetFormulario(e) {
+    formularioEnviar.reset();
+    e.preventDefault();
+}
+
+/// Cuando se envía el correo
+function enviarEmail(e) {
+    // console.log('Mail Enviado');
+    // Spinner al presionar enviar
+    const spinnerGif = document.querySelector('#spinner');
+    spinnerGif.style.display = 'block';
+
+    // Gif que envia email
+    const enviado = document.createElement('img');
+    enviado.src= 'img/mail.gif';
+    enviado.style.display = 'block';
+
+
+    // Ocultar spinner y mostrar gif de enviado
+    setTimeout(function (){
+        spinnerGif.style.display = 'none';
+        document.querySelector('#loaders').appendChild(enviado);
+
+        setTimeout(() => {
+            enviado.remove();
+            formularioEnviar.reset();
+        }, 5000)
+    }, 3000);
+
+    e.preventDefault();
+}
+
+
+/// Verifica la longitud del texto en los campos
 function validarLongitud(campo) {
     // console.log(campo);
     // console.log(campo.value.length);
