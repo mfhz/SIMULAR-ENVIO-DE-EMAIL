72e7cdc001d91b54f467fd83342c0ee8d3f14b84
Mafhz <martinn.henriquez@gmail.com>
2020-08-16 12:52:27 -0500

    Se valida que los campos no estén vacios y si no lo están se habilite el boton submit

--- js/app.js
+++ js/app.js
@@ -10,7 +10,14 @@ const btnEnviar= document.querySelector('#enviar');
 eventListeners();
 
 function eventListeners() {
+    //Inicio de la aplicacion y deshabilitar submit
     document.addEventListener('DOMContentLoaded', inicioApp);
+
+    //Campos del formulario
+    email.addEventListener('blur', validarCampo)
+    asunto.addEventListener('blur', validarCampo)
+    mensaje.addEventListener('blur', validarCampo)
+
 }
 
 
@@ -22,4 +29,39 @@ function inicioApp() {
     btnEnviar.disabled = true;
 
     
+}
+
+/// Valida que el campo tenga algo escrito
+function validarCampo() {
+    // console.log('dentro del Input');
+
+    // Se valida la longitud del texto y que no esté vacio
+    validarLongitud(this);
+    // console.log(this);
+
+    
+
+    let errores = document.querySelectorAll('.error');
+    if (email.value != '' && asunto.value != '' && mensaje.value != '') {
+        if (errores.length === 0) {
+            btnEnviar.disabled = false;            
+        } 
+    } else {
+        btnEnviar.disabled = true;           
+    }
+
+}
+
+function validarLongitud(campo) {
+    // console.log(campo);
+    // console.log(campo.value.length);
+    if (campo.value.length > 0) {
+        // console.log('SI');
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        // console.log('NO');
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
 }
