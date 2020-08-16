ae0628314d5767e874f4069f351baea32f6e8245
Mafhz <martinn.henriquez@gmail.com>
2020-08-16 15:13:42 -0500

    Se verifica el campo email que tenga un @ si no, se deshabilita el submit

--- js/app.js
+++ js/app.js
@@ -39,10 +39,16 @@ function validarCampo() {
     validarLongitud(this);
     // console.log(this);
 
-    
+    // Validar unicamente el email
+    // console.log(this.type);
+    if (this.type === 'email') {
+        validarEmail(this);
+    }
 
     let errores = document.querySelectorAll('.error');
+    // console.log(errores);
     if (email.value != '' && asunto.value != '' && mensaje.value != '') {
+        // console.log(errores.length)
         if (errores.length === 0) {
             btnEnviar.disabled = false;            
         } 
@@ -56,6 +62,21 @@ function validarLongitud(campo) {
     // console.log(campo);
     // console.log(campo.value.length);
     if (campo.value.length > 0) {
+        // console.log('SI');
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    } else {
+        // console.log('NO');
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo) {
+    // console.log(campo);
+    const mensaje = campo.value
+    // console.log(mensaje);
+    if (mensaje.indexOf('@') != -1) {
         // console.log('SI');
         campo.style.borderBottomColor = 'green';
         campo.classList.remove('error');
